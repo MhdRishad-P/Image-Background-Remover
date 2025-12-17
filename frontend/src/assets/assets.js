07cd9573b4ad39ee3c2357148c9f9929622e2475
MuhammedRishad-P <muhammedrishad4319@gmail.com>
2025-12-17 20:03:32 +0530

    Created BgRemovalStep Section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -5,3 +5,30 @@ export const assets={
     logo,
     video_banner
 }
+
+export const steps=[{
+    step:"Step 1",
+    title:"Select an image",
+    description:(
+        `  First, Choose the image you want to remove background from by clicking  on "Start from a photo"
+          Your Image format can be PNG or JPG.
+          we support all image dimensions.`
+        
+        
+    ),
+},
+{
+    step:"Step 2",
+    title:"Let magic remove the background",
+    description:(
+      `Our tool automatically removes the background from your image .Next ,you can choose a background colour .Our most popular options are white and transparent backgrounds ,but you can pick any colour you like`
+    ),
+},
+{
+    step:"Step 3",
+    title:"Download Your Image",
+    description:`After selecting a new background color, download your photo and you're done! 
+    You can also save your picture in the Photoroom App by creating an  account`
+
+}
+]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
+import BgRemovalSteps from '../components/BgRemovalSteps'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header/>
 
       {/* Background Removal steps  section*/}
+      <BgRemovalSteps/>
 
       {/* Background Removal  slider section */}
 
