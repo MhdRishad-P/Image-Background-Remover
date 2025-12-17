0c4972dc954db5feca85f55fbf285136bddc7e17
MuhammedRishad-P <muhammedrishad4319@gmail.com>
2025-12-17 19:23:21 +0530

    Created Header Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menubar from './components/Menubar'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div >
       <Menubar/>
+      <Home/>
     </div>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 font-[Outfit]'>
+      {/* Hero Section */}
+      <Header/>
+
+      {/* Background Removal steps  section*/}
+
+      {/* Background Removal  slider section */}
+
+      {/* Buy Credits plan section */}
+
+      {/* User testimonial section */}
+
+      {/* Try now Section */}
+
+    </div>
+  )
+}
+
+export default Home
